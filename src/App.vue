8a5006ded083c0484f25d788e2f38dd1b20a207f
Del1rius <timothy.aidan.barry@gmail.com>
2023-06-30 16:09:23 +0200

    baseline

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBarComp/>
   <router-view/>
 </template>
+
+<script>
+import NavBarComp from "./components/NavBar-Comp.vue";
+ 
+export default {
+  components: {NavBarComp}
+}
+</script>
+
 
 <style>
 #app {
@@ -15,16 +21,11 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+*{
+  margin: 0;
+  padding: 0;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+
 </style>
---
+++ src/components/NavBar-Comp.vue
@@ -0,0 +1,29 @@
+<template >
+    <div class="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/Contact">Contact</router-link>
+        <router-link to=""></router-link>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+div {
+    padding: 30px;
+    background-color: #008080;
+}
+
+a {
+    text-decoration: none;
+    color: white;
+    text-align: center;
+    font-size: 25px;
+    padding: 20px;
+    margin: 20px;
+}
+    
+</style>
