0d2e0e679a43a498ee5f7de7294b8660cff69bf9
Timothy Barry <99873394+Del1rius@users.noreply.github.com>
2023-11-13 16:19:13 +0200

    Update Footer-Comp.vue

--- src/components/Footer-Comp.vue
+++ src/components/Footer-Comp.vue
@@ -1,10 +1,10 @@
 <template >
     <div class="footer">
     <div>
-    <a href="https://www.linkedin.com/in/timothy-barry-82344b262" ><img src="https://i.postimg.cc/DzmPwk8q/icons8-linkedin-50.png" alt="linkedin" target=_blank></a>
-    <a href="https://github.com/Del1rius"><img src="https://i.postimg.cc/Y9L9986n/icons8-github-50.png" alt="github" target=_blank></a>
-    <a href="https://www.instagram.com/tim.has.adhd/"><img src="https://i.postimg.cc/tR2xMvrP/icons8-instagram-50.png" alt="instagram" target=_blank></a>
-    <a href="https://www.facebook.com/timothy.barry.75098/" blank><img src="https://i.postimg.cc/4dszKBb1/icons8-facebook-50.png" alt="facebook" target=_blank></a>
+    <a href="https://www.linkedin.com/in/timothy-barry-82344b262" ><img src="https://i.postimg.cc/DzmPwk8q/icons8-linkedin-50.png" alt="linkedin" target="_blank"></a>
+    <a href="https://github.com/Del1rius"><img src="https://i.postimg.cc/Y9L9986n/icons8-github-50.png" alt="github" target="_blank"></a>
+    <a href="https://www.instagram.com/tim.has.adhd/"><img src="https://i.postimg.cc/tR2xMvrP/icons8-instagram-50.png" alt="instagram" target="_blank"></a>
+    <a href="https://www.facebook.com/timothy.barry.75098/" blank><img src="https://i.postimg.cc/4dszKBb1/icons8-facebook-50.png" alt="facebook" target="_blank"></a>
     </div>
     <p class="text-end">&copy; Made By Tim - 2023</p>        
     </div>
